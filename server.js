741d919b6fbe88a25bf67297c3f0c9264eae6572
Archil Imnadze <archil@imnadze.ge>
2016-06-11 11:32:30 -0400

    issue a token

--- server.js
+++ server.js
@@ -5,8 +5,11 @@ var config = require('./config.js'),
     Error404Page = require('./lib/Error404Page.js'),
     Log = require('./lib/Log.js')
 
+var sendingFiles = Object.create(null)
+
 var pages = Object.create(null)
 pages['/'] = require('./lib/IndexPage.js')
+pages['/sessionNode/send'] = require('./lib/SendPage.js')(sendingFiles)
 
 http.createServer((req, res) => {
     Log.http(req.method + ' ' + req.url)
