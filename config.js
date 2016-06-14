6938890ca256b537ab17e11fd81d0ab4aa93a177
Archil Imnadze <archil@imnadze.ge>
2016-06-14 13:57:30 +0000

    notify account node about receiving file

--- config.js
+++ config.js
@@ -1,6 +1,11 @@
 exports.port = 7500
 exports.host = '127.0.0.1'
 
+exports.accountNode = {
+    host: '127.0.0.1',
+    port: 7300,
+}
+
 exports.sessionNode = {
     host: '127.0.0.1',
     port: 7400,
